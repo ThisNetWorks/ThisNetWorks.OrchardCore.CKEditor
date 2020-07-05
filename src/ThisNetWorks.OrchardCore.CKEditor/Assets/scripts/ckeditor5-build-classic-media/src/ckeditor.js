10e9d704c561e5ad94fafa734682aee2d39ed37c
Dean Marcussen <dean.marcussen@gmail.com>
2020-07-05 18:47:22 +0100

    Preview working, stripped other image plugins from ckeditor

--- src/ThisNetWorks.OrchardCore.CKEditor/Assets/scripts/ckeditor5-build-classic-media/src/ckeditor.js
+++ src/ThisNetWorks.OrchardCore.CKEditor/Assets/scripts/ckeditor5-build-classic-media/src/ckeditor.js
@@ -7,23 +7,15 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
@@ -38,23 +30,15 @@ export default class ClassicEditor extends ClassicEditorBase {}
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
 	BlockQuote,
 	CKFinder,
-	EasyImage,
 	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
 	Indent,
 	Link,
 	List,
-	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
 	Table,
@@ -79,20 +63,10 @@ ClassicEditor.defaultConfig = {
 			'outdent',
 			'|',
 			'insertImage',
-			'imageUpload',
 			'blockQuote',
 			'insertTable',
-			'mediaEmbed',
 			'undo',
 			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
 		]
 	},
 	table: {
--- src/ThisNetWorks.OrchardCore.CKEditor/Assets/scripts/ckeditor5-build-classic-media/src/orchardcore-media.js
+++ src/ThisNetWorks.OrchardCore.CKEditor/Assets/scripts/ckeditor5-build-classic-media/src/orchardcore-media.js
@@ -5,7 +5,6 @@ import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 
 export default class InsertAsset extends Plugin {
     init() {
-        console.log( 'InsertAsset was initialized' );
         const editor = this.editor;
 
         editor.ui.componentFactory.add( 'insertImage', locale => {
@@ -19,7 +18,6 @@ export default class InsertAsset extends Plugin {
 
             // Callback executed once the image is clicked.
             view.on( 'execute', () => {
-                // const imageURL = prompt( 'Image URL' );
 
                 $("#mediaApp").detach().appendTo('#mediaModalBody .modal-body');
                 $("#mediaApp").show();
@@ -28,27 +26,21 @@ export default class InsertAsset extends Plugin {
                 //disable an reset on click event over the button to avoid issue if press button multiple times or have multiple editor
                 $('#mediaHtmlFieldSelectButton').off('click');
                 $('#mediaBodySelectButton').on('click', function (v) {
-                    // trumbowyg.restoreRange();
-                    // trumbowyg.range.deleteContents();
+
+                    var content = '';
                     
                     for (i = 0; i < mediaApp.selectedMedias.length; i++) {
                         var mediaBodyContent = ' [image]' + mediaApp.selectedMedias[i].mediaPath + '[/image]';
-                        console.log(mediaBodyContent);
-                        // var node = document.createTextNode(mediaBodyContent);
-                        // trumbowyg.range.insertNode(node);
+                        content = content  + mediaBodyContent;
                     }
-                    
-                    // trumbowyg.syncCode();
-                    // trumbowyg.$c.trigger('tbwchange');
-                    // //avoid tag to be selected after add it
-                    // trumbowyg.$c.focus();
 
+                    editor.model.change( writer => {
+                        editor.model.insertContent(writer.createText( content ), editor.model.document.selection);
+                    } );
+                    
                     $('#mediaModalBody').modal('hide');
                     return true;
                 });
-
-
-
 
             } );
 
