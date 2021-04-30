26e01f780494f7fd219bdf85fadbc305a9b42264
Dean Marcussen <dean.marcussen@gmail.com>
2021-04-30 10:49:34 +0100

    Upgrade OC (#20)

--- samples/ThisNetWorks.OrchardCore.CKEditor.Sample.Web/Startup.cs
+++ samples/ThisNetWorks.OrchardCore.CKEditor.Sample.Web/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using OrchardCore.ResourceManagement;
 using ThisNetWorks.OrchardCore.CKEditor.Sample.Web.ResourceManifests;
 
@@ -14,10 +15,10 @@ namespace ThisNetWorks.OrchardCore.CKEditor.Sample.Web
 
                 /// This is an example of how to override the <script asp-name="ckeditorclassic"> tag helper
                 /// Register a IResourceManifestProvider with the urls to your custom build of CKEditor and it will replace the default custom build registration in the editor view.             
-                // .ConfigureServices(tenantServices => tenantServices.AddScoped<IResourceManifestProvider, ClassicEditorResourceManifest>())
+                .ConfigureServices(tenantServices => tenantServices.AddTransient<IConfigureOptions<ResourceManagementOptions>, ClassicEditorResourceManagementOptionsConfiguration>())
 
                 // This registers a custom style sheet for the admin with some specific styles for using in the CKEditor with a custom paragraph / heading toolbar.
-                .ConfigureServices(tenantServices => tenantServices.AddScoped<IResourceManifestProvider, CKEditorTheAdminResourceManifest>())
+                .ConfigureServices(tenantServices => tenantServices.AddTransient<IConfigureOptions<ResourceManagementOptions>, CKEditorTheAdminResourceManagementOptionsConfiguration>())
                 ;
         }
         
